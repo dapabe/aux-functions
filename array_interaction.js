4036165842893ef7d4c805e0ca9463e01743c1a2
DanBec <65568682+dapabe@users.noreply.github.com>
2022-04-10 16:32:32 -0300

    Update array_interaction.js
    added use case for the 1st function

--- array_interaction.js
+++ array_interaction.js
@@ -1,6 +1,6 @@
 //  Takes 2 array of objects as parameters, look for a key in
-//  the 1st array and returns an object with the specified
-//  property from the 2nd array.
+//  the 1st array, find if it exists in the 2nd array and returns
+//  an object with the specified property from the 2nd array.
 function CompareAndRetrieve({
   aLook,
   aSearch,
@@ -15,3 +15,22 @@ function CompareAndRetrieve({
   });
   return returnedValues.length !== 0 ? returnedValues : null;
 }
+//  EX:
+//  const skills = 
+//  [
+//    { name: "HTML" },
+//    { name: "CSS" },
+//  ]
+//  const icons = 
+//  [
+//    { name: "HTML", file: "HTML.svg", img_desc: "HTML5 Logo" },
+//    { name: "CSS", file: "CSS.svg", img_desc: "CSS Logo" },
+//    { name: "NodeJS", file: "NodeJS.svg", img_desc: "NodeJS Logo" },
+//  ]
+//    [use case]
+//  CompareAndRetrieve({
+//  aLook: skills,
+//  aSearch: icons,
+//  kSearch: "name",
+//  kReturn: "file"
+//  }).forEach((item)=> console.log(i)) --> Should log every coincidence.
