c4363b2980715a8db85d06ea9c2082d9ba9165b5
Daniel Becerra <65568682+dapabe@users.noreply.github.com>
2022-04-26 20:42:21 -0300

    Update useFetch.jsx

--- JSX  - JavaScript XML/hooks/useFetch.jsx
+++ JSX  - JavaScript XML/hooks/useFetch.jsx
@@ -1,67 +1,31 @@
-import { useEffect, useState } from "react";
+//  TODO: NOTES
 
+const responseType = {
+  clientRes: null,
+  serverRes: null,
+};
 
-//  REFACTOR
-//                                                          Optional
-export default async function useFetch({ POST = false, url, postResponse }) {
-  const [response, setResponse] = useState(null);
-  const [error, setError] = useState(null);
+export default function useFetch(url) {
+  const [response, setResponse] = useState(responseType);
   const [isLoading, setLoading] = useState(false);
-  const [inputData, setInputData] = useState(null);
 
-  //  [NOTE]  Using Computing Properties
-  //          to overwrite previous data
-  //          from the state.
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setInputData({ ...inputData, [name]: value });
-  };
-
-  const formSubmit = (event) => {
-    event.preventDefault();
-
-    try {
-      await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-          Accept: "application/json",
-        },
-        body: JSON.stringify(inputData),
-      });
-      setResponse(postResponse.ok); //  Needs testing
-      setLoading(false);
-      setError(null);
-    } catch (error) {
-      setResponse(postResponse.error);
-      setLoading(false);
-      setError(error);
-    }
-  };
-
-  useEffect(() => {
+  useEffect(async () => {
     setLoading(true);
-    setError(null);
 
-    //  fetchs defaults to GET, POST = false = GET
-    if (!POST) {
-      return await fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-          setResponse(data);
-          setLoading(false);
-          setError(null);
-        })
-        .catch((error) => {
-          setResponse(null);
-          setLoading(false);
-          setError(error);
-        });
-    } else formSubmit();
+    //  refactor setResponses
+    await fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setResponse({ ...response, data });
+        setLoading(false);
+      })
+      .catch((error) => {
+        setResponse(error);
+        setLoading(false);
+      });
 
-    return () => setResponse(null), setError(null), setLoading(false);
-  }, []);
+    return () => setResponse(null), setLoading(false);
+  }, [url]);
 
-  //   GET/POST.response               POST       onChange      onSubmit
-  return { response, error, isLoading, inputData, handleChange, formSubmit };
+  return { response, isLoading };
 }
