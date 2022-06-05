b5432ce8c21d5000d26ab23d92e2e1d03288cc6b
DanBec <65568682+dapabe@users.noreply.github.com>
2022-06-05 00:11:10 -0300

    deleted jsx directory

--- JSX  - JavaScript XML/hooks/useToggle.js
+++
@@ -1,9 +0,0 @@
-function useToggle(init = false) {
-  const [initialValue, setValue] = useState(init);
-
-  // Define and memorize toggler function,
-  // This function change the boolean value to it's opposite value
-  const toggle = useCallback(() => setValue((prevState) => !prevState), []);
-
-  return [initialValue, toggle];
-}
